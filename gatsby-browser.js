a62578351fb0feb3ac75ee944f9958de06c54e48
阿月很乖 <lizhongyue248@163.com>
2021-04-10 16:50:36 +0800

    :wheelchair: PWA update tip.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,13 @@
 require('./src/styles/global.css')
 
 module.exports.wrapRootElement = require('./gatsby-ssr').wrapRootElement
+
+exports.onServiceWorkerUpdateFound = () => {
+  if (
+    window.confirm(
+      '此网站已使用新数据进行了更新。是否要重新加载站点以获取新数据？'
+    )
+  ) {
+    window.location.reload(true)
+  }
+}
